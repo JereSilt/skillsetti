525140270d3d25b483bf7c7af4ec3fdee49a3dd3
JereSilt <jere.siltakoski@hotmail.fi>
2024-02-06 15:10:48 +0200

    finalWorking

--- App.js
+++ App.js
@@ -14,6 +14,48 @@ export default function App() {
   const [values, setValues] = useState(new Array(SKILLS.length).fill(0));
   const [average, setAverage] = useState(0);
 
+  useEffect(() => {
+    calculateAverageSkill(); 
+  }, [values]);
+
+  const items = [];
+  for (let i = 0; i < SKILLS.length; i++) {
+    items.push(
+      <View key={"item" + i} style={styles.skills}>
+        <Text style={styles.skill}>{SKILLS[i]}</Text>
+        <Text style={styles.value}>Skill: {values[i]}</Text>
+        <Container fluid>
+          <Row>
+            <Col><Text style={styles.min}>{MIN}</Text></Col>
+            <Col xs="9">
+              <Slider
+                minimumValue={MIN}
+                maximumValue={MAX}
+                step={1}
+                value={values[i]}
+                minimumTrackTintColor="#006666"
+                maximumTrackTintColor="#ff9900"
+                onValueChange={(val) => setSkillValue(val, i)}/>
+            </Col>
+            <Col><Text>{MAX}</Text></Col>
+          </Row>
+        </Container>
+      </View>
+    );
+  }
+
+  const setSkillValue = (val, i) => {
+    let skillValues = [...values];
+    skillValues[i] = val;
+    setValues(skillValues);
+  }
+
+  const calculateAverageSkill = () => {
+    const sum = values.reduce((a, b) => a + b, 0);
+    const avg = (sum / values.length) || 0;
+    setAverage(avg);
+  }
+
   return (
     <View style={styles.container}>
       <Text>Value: {value}</Text>
@@ -35,7 +77,39 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 40,
   },
+  header: {
+    textAlign: 'center',
+    marginTop: 20,
+    fontSize: 30,
+    fontWeight: 'bold'
+  },
+  skills: {
+    alignItems: 'center',
+  },
+  skill: {
+    marginTop: 30,
+    fontSize: 25
+  },
+  min: {
+    marginLeft: 10
+  },
+  value: {
+    marginTop: 5,
+    marginBottom: 5,
+    fontSize: 20
+  },
+  averageHeader: {
+    textAlign: 'center',
+    marginTop: 10,
+    fontSize: 25,
+  },
+  averageValue: {
+    textAlign: 'center',
+    marginTop: 10,
+    fontSize: 40,
+  }
+
 });
